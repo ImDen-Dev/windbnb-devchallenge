d63605cd06ec9961301515bda33c80fd1c2a61e9
ImDen <neckromant90@gmail.com>
2021-01-20 16:50:32 +0200

    new fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/windbnb"));
 
 app.listen(process.env.PORT || 8080);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/windbnb/index.html"));
 });
 
 console.log("Console listening!");
